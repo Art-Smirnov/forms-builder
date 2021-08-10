2ad0638867ca2000b974f65ed28d61bb51c06559
Artem Smirnov <aratatem2013@gmail>
2021-08-10 18:52:30 +0300

    fix: fixed all eslint errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    NoopAnimationsModule
-  ],
+  declarations: [AppComponent],
+  imports: [MatSliderModule, BrowserModule, NoopAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
