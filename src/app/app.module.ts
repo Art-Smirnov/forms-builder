ce375f0df74c46cbdca99df56d41ef460a0a7e4e
Artem Smirnov <aratatem2013@gmail>
2021-08-11 12:28:46 +0300

    feat: created button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { AccordionComponent } from './accordion/accordion.component';
 import { DropSectionComponent } from './drop-section/drop-section.component';
 import { DragSectionComponent } from './drag-section/drag-section.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
-  declarations: [AppComponent, AccordionComponent, DropSectionComponent, DragSectionComponent],
+  declarations: [AppComponent, AccordionComponent, DropSectionComponent, DragSectionComponent, ButtonComponent],
   imports: [BrowserModule, NoopAnimationsModule, CdkAccordionModule],
   providers: [],
   bootstrap: [AppComponent],
