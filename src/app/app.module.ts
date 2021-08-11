53424127f549e64714e34e9a1af52c0f8b878d70
Artem Smirnov <aratatem2013@gmail>
2021-08-11 10:52:05 +0300

    install: install normalize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
 import { AppComponent } from './app.component';
 import { AccordionComponent } from './accordion/accordion.component';
 import { DropSectionComponent } from './drop-section/drop-section.component';
@@ -10,7 +9,7 @@ import { DragSectionComponent } from './drag-section/drag-section.component';
 
 @NgModule({
   declarations: [AppComponent, AccordionComponent, DropSectionComponent, DragSectionComponent],
-  imports: [MatSliderModule, BrowserModule, NoopAnimationsModule],
+  imports: [BrowserModule, NoopAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
