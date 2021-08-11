52e48d99bb4476691ef95ff95a04f99767faea1a
Artem Smirnov <aratatem2013@gmail>
2021-08-11 10:24:56 +0300

    feat: create drop-section component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { AppComponent } from './app.component';
 import { AccordionComponent } from './accordion/accordion.component';
+import { DropSectionComponent } from './drop-section/drop-section.component';
 
 @NgModule({
-  declarations: [AppComponent, AccordionComponent],
+  declarations: [AppComponent, AccordionComponent, DropSectionComponent],
   imports: [MatSliderModule, BrowserModule, NoopAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
