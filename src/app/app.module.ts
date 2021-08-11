65196b3bdc8b45b00abdd77b2a53cdbaeac12bde
Artem Smirnov <aratatem2013@gmail>
2021-08-11 12:48:23 +0300

    feat: add button from material angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { AccordionComponent } from './accordion/accordion.component';
 import { DropSectionComponent } from './drop-section/drop-section.component';
@@ -10,8 +11,14 @@ import { DragSectionComponent } from './drag-section/drag-section.component';
 import { ButtonComponent } from './button/button.component';
 
 @NgModule({
-  declarations: [AppComponent, AccordionComponent, DropSectionComponent, DragSectionComponent, ButtonComponent],
-  imports: [BrowserModule, NoopAnimationsModule, CdkAccordionModule],
+  declarations: [
+    AppComponent,
+    AccordionComponent,
+    DropSectionComponent,
+    DragSectionComponent,
+    ButtonComponent,
+  ],
+  imports: [BrowserModule, NoopAnimationsModule, CdkAccordionModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
